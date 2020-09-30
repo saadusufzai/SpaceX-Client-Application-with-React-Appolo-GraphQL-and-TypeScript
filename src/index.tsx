7a3d77a18dc89ccd963e6166d149c9162ca346d3
Saad Aslam <saadusufzai@gmail.com>
2020-09-30 14:44:04 +0500

    app rendering
    finally

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient, { InMemoryCache } from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
+// import ApolloClient, { InMemoryCache } from 'apollo-boost';
+// import { ApolloProvider } from 'react-apollo';
 import { ApolloProvider as ApolloHooksProvider } from 'react-apollo-hooks';
+
+import { ApolloClient, InMemoryCache ,ApolloProvider} from '@apollo/client';
+
 import './index.css';
 import App from './App';
 
@@ -13,9 +16,9 @@ const client = new ApolloClient({
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <ApolloHooksProvider client={client}>
+    
       <App />
-    </ApolloHooksProvider>
+    
   </ApolloProvider>,
   document.getElementById('root'),
 );
