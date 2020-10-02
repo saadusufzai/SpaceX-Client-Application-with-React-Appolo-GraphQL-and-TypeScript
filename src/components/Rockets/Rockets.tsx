c19d1ce02f17fba7e437316702ed0183f0128d76
Saad Aslam <saadusufzai@gmail.com>
2020-10-02 18:12:30 +0500

    rockers.tsx update

---
+++ src/components/Rockets/Rockets.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles'
+
+const Rockets = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Rockets
---
+++ src/components/Rockets/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RocketContainer = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RocketContainer
